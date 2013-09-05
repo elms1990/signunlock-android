6139a812bc51e7be9521620d4b64f85d265dc879
Felix Dumit <felix.dumit@gmail.com>
2013-09-05 16:20:00 -0300

    trying to upload sample project!

---
+++ SignUnlock/src/main/java/br/unicamp/signunlock/MainActivity.java
@@ -0,0 +1,26 @@
+package br.unicamp.signunlock;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView t = (TextView) findViewById(R.id.text1);
+        t.setText("Unlock me!");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
